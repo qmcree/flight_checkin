34a11753f920e5484d5bcd9181415f4aaeab7ed8
mcreemq <mcreemq@ptk.org>
2014-05-02 15:16:33 -0500

    na

--- app/routes.php
+++ app/routes.php
@@ -26,7 +26,7 @@ Route::post('reservation/{id}/delete', 'ReservationController@delete');
 
 // @todo remove.
 Route::get('debug', function() {
-    $flight = Flight::with('airline', 'airport.timezone', 'reservation')->find(3);
+    $flight = Flight::with('airline', 'reservation')->find(3);
     var_dump($flight);
     return;
 });
