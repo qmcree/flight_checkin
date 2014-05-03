dfc8f1f8d95402094436703dce3ba6fddf242ac9
Quentin McRee <imqtek@gmail.com>
2014-05-03 05:20:34 -0500

    na

--- app/controllers/ReservationController.php
+++ app/controllers/ReservationController.php
@@ -16,7 +16,26 @@ class ReservationController extends BaseController
 
     public function create()
     {
+        $airport = Airport::with('timezone')->find($_POST['airport_id']);
+        $timezone = $airport['relations']['timezone']['attributes']['name'];
 
+        $date = new DateTime($_POST['date'], new DateTimeZone($timezone));
+        $utcDate = gmdate('Y-m-d H:i:s', $date->getTimestamp());
+        echo $utcDate;
+        exit;
+
+        $flight = Flight::create(array(
+            'date' => $_POST['date'],
+            'airline_id' => 1, // always southwest.
+            'airport_id' => $_POST['airport_id'],
+        ));
+
+        Reservation::create(array(
+            'flight_id' => $flight->id,
+            'confirmation_number' => $_POST['confirmation_number'],
+            'first_name' => $_POST['first_name'],
+            'last_name' => $_POST['last_name'],
+        ));
     }
 
     public function showEditForm($id)
