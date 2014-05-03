b78e8546982d6856c435aeb91e86e1b03268d732
Quentin McRee <imqtek@gmail.com>
2014-05-03 00:58:37 -0500

    na

--- app/commands/CheckinCommand.php
+++ app/commands/CheckinCommand.php
@@ -40,8 +40,7 @@ class CheckinCommand extends Command {
         $upcomingFlights = Flight::with('airline', 'airport.timezone', 'reservation')->upcoming()->get();
 
         foreach ($upcomingFlights as $flight) {
-            //$this->info(var_export(Checkin::attempt($flight), true));
-            $this->info('Little testy testy.');
+            $this->info(var_export(Checkin::attempt($flight), true));
         }
 	}
 }
