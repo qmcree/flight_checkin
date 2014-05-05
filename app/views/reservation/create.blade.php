d8476875db040d5fe885c6381069a4cf0a622ce1
mcreemq <mcreemq@ptk.org>
2014-05-05 17:16:09 -0500

    na

--- app/views/reservation/create.blade.php
+++ app/views/reservation/create.blade.php
@@ -1,26 +1,29 @@
 @extends('layout.master')
 
 @section('content')
+    <h2>New Reservation</h2>
+
     <form method="post" action="{{ action('ReservationController@create') }}">
         <div class="form-group">
             <label for="date">Date</label>
-            <input type="datetime" name="date" id="date" />
+            <input type="datetime" class="form-control" name="date" id="date" />
         </div>
         <div class="form-group">
             <label for="confirmation-number">Confirmation Number</label>
-            <input type="text" name="confirmation_number" id="confirmation-number" />
+            <input type="text" class="form-control" name="confirmation_number" id="confirmation-number" />
         </div>
         <div class="form-group">
             <label for="first-name">First Name</label>
-            <input type="text" name="first_name" id="first-name" />
+            <input type="text" class="form-control" name="first_name" id="first-name" />
         </div>
         <div class="form-group">
             <label for="last-name">Last Name</label>
-            <input type="text" name="last_name" id="last-name" />
+            <input type="text" class="form-control" name="last_name" id="last-name" />
         </div>
         <div class="form-group">
             <label for="email">Email</label>
-            <input type="email" name="email" id="email" />
+            <input type="email" class="form-control" name="email" id="email" />
+            <p class="help-block">We'll shoot you an email right after we check you in.</p>
         </div>
         <div class="form-group">
             <label for="timezone">Departure Airport Timezone</label>
@@ -31,7 +34,7 @@
                 @endforeach
             </select>
         </div>
-        <input type="submit" name="submit" />
+        <button type="submit" class="btn btn-primary">Create</button>
     </form>
     <p><small><a href="{{ action('ReservationController@lookup') }}">Trying to change an existing reservation?</a></small></p>
 @stop
--- app/views/reservation/lookup.blade.php
+++ app/views/reservation/lookup.blade.php
@@ -1,19 +1,21 @@
 @extends('layout.master')
 
 @section('content')
+    <h2>Lookup Your Reservation</h2>
+
     <form method="get" action="{{ action('ReservationController@lookup') }}">
         <div class="form-group">
             <label for="confirmation-number">Confirmation Number</label>
-            <input type="text" name="confirmation_number" id="confirmation-number" />
+            <input type="text" class="form-control" name="confirmation_number" id="confirmation-number" />
         </div>
         <div class="form-group">
             <label for="first-name">First Name</label>
-            <input type="text" name="first_name" id="first-name" />
+            <input type="text" class="form-control" name="first_name" id="first-name" />
         </div>
         <div class="form-group">
             <label for="last-name">Last Name</label>
-            <input type="text" name="last_name" id="last-name" />
+            <input type="text" class="form-control" name="last_name" id="last-name" />
         </div>
-        <input type="submit" name="submit" />
+        <button type="submit" class="btn btn-default">Lookup</button>
     </form>
 @stop
