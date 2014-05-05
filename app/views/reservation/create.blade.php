dda3529b1543f98f8e3de23a88a31869913f5bba
Quentin McRee <imqtek@gmail.com>
2014-05-05 01:45:14 -0500

    na

--- app/views/reservation/create.blade.php
+++ app/views/reservation/create.blade.php
@@ -1,44 +1,36 @@
-@if (!is_null($_success))
-<div class="alert alert-success">
-    {{ $_success }}
-</div>
-@endif
+@extends('layout.base')
 
-@if (!is_null($_danger))
-<div class="alert alert-danger">
-    {{ $_danger }}
-</div>
-@endif
-
-<form method="post" action="{{ action('ReservationController@create') }}">
-    <div class="form-group">
-        <label for="date">Date</label>
-        <input type="datetime" name="date" id="date" />
-    </div>
-    <div class="form-group">
-        <label for="confirmation-number">Confirmation Number</label>
-        <input type="text" name="confirmation_number" id="confirmation-number" />
-    </div>
-    <div class="form-group">
-        <label for="first-name">First Name</label>
-        <input type="text" name="first_name" id="first-name" />
-    </div>
-    <div class="form-group">
-        <label for="last-name">Last Name</label>
-        <input type="text" name="last_name" id="last-name" />
-    </div>
-    <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" />
-    </div>
-    <div class="form-group">
-        <label for="timezone">Departure Airport Timezone</label>
-        <select name="timezone_id" id="timezone">
-            <option value="">Select...</option>
-            @foreach ($timezones as $timezone)
-            <option value="{{ $timezone['attributes']['id'] }}">{{{ $timezone['attributes']['name'] }}}</option>
-            @endforeach
-        </select>
-    </div>
-    <input type="submit" name="submit" />
-</form>
+@section('content')
+    <form method="post" action="{{ action('ReservationController@create') }}">
+        <div class="form-group">
+            <label for="date">Date</label>
+            <input type="datetime" name="date" id="date" />
+        </div>
+        <div class="form-group">
+            <label for="confirmation-number">Confirmation Number</label>
+            <input type="text" name="confirmation_number" id="confirmation-number" />
+        </div>
+        <div class="form-group">
+            <label for="first-name">First Name</label>
+            <input type="text" name="first_name" id="first-name" />
+        </div>
+        <div class="form-group">
+            <label for="last-name">Last Name</label>
+            <input type="text" name="last_name" id="last-name" />
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" />
+        </div>
+        <div class="form-group">
+            <label for="timezone">Departure Airport Timezone</label>
+            <select name="timezone_id" id="timezone">
+                <option value="">Select...</option>
+                @foreach ($timezones as $timezone)
+                <option value="{{ $timezone['attributes']['id'] }}">{{{ $timezone['attributes']['name'] }}}</option>
+                @endforeach
+            </select>
+        </div>
+        <input type="submit" name="submit" />
+    </form>
+@stop
--- app/views/reservation/lookup.blade.php
+++ app/views/reservation/lookup.blade.php
@@ -1,27 +1,19 @@
-@if (!is_null($_success))
-<div class="alert alert-success">
-    {{ $_success }}
-</div>
-@endif
+@extends('layout.base')
 
-@if (!is_null($_danger))
-<div class="alert alert-danger">
-    {{ $_danger }}
-</div>
-@endif
-
-<form method="get" action="{{ action('ReservationController@lookup') }}">
-    <div class="form-group">
-        <label for="confirmation-number">Confirmation Number</label>
-        <input type="text" name="confirmation_number" id="confirmation-number" />
-    </div>
-    <div class="form-group">
-        <label for="first-name">First Name</label>
-        <input type="text" name="first_name" id="first-name" />
-    </div>
-    <div class="form-group">
-        <label for="last-name">Last Name</label>
-        <input type="text" name="last_name" id="last-name" />
-    </div>
-    <input type="submit" name="submit" />
-</form>
+@section('content')
+    <form method="get" action="{{ action('ReservationController@lookup') }}">
+        <div class="form-group">
+            <label for="confirmation-number">Confirmation Number</label>
+            <input type="text" name="confirmation_number" id="confirmation-number" />
+        </div>
+        <div class="form-group">
+            <label for="first-name">First Name</label>
+            <input type="text" name="first_name" id="first-name" />
+        </div>
+        <div class="form-group">
+            <label for="last-name">Last Name</label>
+            <input type="text" name="last_name" id="last-name" />
+        </div>
+        <input type="submit" name="submit" />
+    </form>
+@stop
