f3f9bb325f0b4c633230c504fa4a49b4ccb726b8
Quentin McRee <imqtek@gmail.com>
2014-05-09 01:59:42 -0500

    na.

--- app/views/reservation/edit.blade.php
+++ app/views/reservation/edit.blade.php
@@ -41,7 +41,9 @@
             <p class="help-block">What's the timezone of the airport you'll be departing from?
                 <a href="/packages/flight_checkin/images/timezone_map.gif" target="_blank">I dunno.</a></p>
         </div>
-        <button type="button" class="btn btn-danger pull-right">Delete</button>
+        <button type="button" class="btn btn-danger pull-right">
+            <a href="#" class="delete" data-type="reservation">Delete</a>
+        </button>
         <button type="submit" class="btn btn-primary">Save</button>
         <button type="reset" class="btn btn-default">Reset</button>
     </form>
--- public/packages/flight_checkin/js/flight_checkin.js
+++ public/packages/flight_checkin/js/flight_checkin.js
@@ -4,4 +4,14 @@ jQuery(document).ready(function() {
         autoclose: true,
         minuteStep: 15
     });
+
+    jQuery('a.delete').click(function(e) {
+        e.preventDefault();
+
+        var link = jQuery(this);
+
+        if (window.confirm('Are you sure you want to delete this ' + link.data('type') + '? This is irreversible.')) {
+            window.location = link.prop('href');
+        }
+    });
 });
